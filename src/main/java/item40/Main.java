49799d4100cf975e12b15ee50a13745ff319977a
paulo <paulo@JR-LENOVO>
2025-02-08 12:18:17 -0300

    item40 Expressão condicional ternária

---
+++ src/main/java/item40/Main.java
@@ -0,0 +1,28 @@
+package item40;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US); // Define o formato de números para o padrão dos EUA
+        Scanner sc = new Scanner(System.in);
+
+        // Expressão condicional ternária
+        double preco = 34.5; // Define o preço do produto como 34.5
+        
+        // A linha abaixo calcula o desconto usando a expressão condicional ternária:
+        // (preco < 20) ? preco * 0.1 : preco * 0.05;
+        // - Se a condição (preco < 20) for verdadeira, calcula o desconto como preco * 0.1 (10%).
+        // - Caso contrário (condição falsa), calcula o desconto como preco * 0.05 (5%).
+        double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+        // Exibe o valor do desconto calculado
+        System.out.println("desconto = " + desconto);
+
+        sc.close(); // Fecha o objeto Scanner para liberar recursos
+    }
+
+}
