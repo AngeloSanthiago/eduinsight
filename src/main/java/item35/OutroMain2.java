40fa8774e580073194eb7882a9855335b63eb13b
leyanedev <leyane.ti2023@gmail.com>
2025-02-15 00:51:29 -0300

    item35teste concluido testando meus conhecimentos

---
+++ src/main/java/item35/OutroMain2.java
@@ -0,0 +1,17 @@
+package item35;
+
+public class OutroMain2 {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		
+		System.out.println("Meu nome é Leyane");
+		if (x > 0) {
+		System.out.println("Sou professora de Espanhol");
+		}
+		System.out.println("Trabalho no Colégio Dom José");
+		
+}
+	
+
+}
