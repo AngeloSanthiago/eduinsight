0c99d09b3f8b9dcaf6ad1ac4ee862a1319ae8ab1
Angelo Santhiago <angelosanthiago@gmail.com>
2025-02-17 22:33:51 -0300

    item-50 topo - inclusões e alterações do item 40

--- src/main/java/item40/Main.java
+++ src/main/java/item40/Main.java
@@ -1,28 +1,21 @@
 package item40;
 
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-
-        Locale.setDefault(Locale.US); // Define o formato de números para o padrão dos EUA
-        Scanner sc = new Scanner(System.in);
-
-        // Expressão condicional ternária
-        double preco = 34.5; // Define o preço do produto como 34.5
-        
-        // A linha abaixo calcula o desconto usando a expressão condicional ternária:
-        // (preco < 20) ? preco * 0.1 : preco * 0.05;
-        // - Se a condição (preco < 20) for verdadeira, calcula o desconto como preco * 0.1 (10%).
-        // - Caso contrário (condição falsa), calcula o desconto como preco * 0.05 (5%).
-        double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
-
-        // Exibe o valor do desconto calculado
-        System.out.println("desconto = " + desconto);
-
-        sc.close(); // Fecha o objeto Scanner para liberar recursos
-    }
+	public static void main(String[] args) {
+		double preco = 34.5;
+		double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+		
+//		if (preco == 30) {
+//			System.out.println("Preço é menor que R$ 30,00");
+//		}
+		
+//		if (preco < 20.0) {
+//			desconto = preco * 0.1; // Desconto de 10%
+//		} else {
+//			desconto = preco * 0.05; // Desconto de 5%
+//		}
+		
+		System.out.println("Valor do desconto calculado: R$" + desconto);
+	}
 
 }
