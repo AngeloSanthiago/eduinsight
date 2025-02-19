aa602367ed8da9a7ec80b7d53a9e079a877e762e
paulo <paulo@JR-LENOVO>
2025-02-19 13:32:07 -0300

    item59 part2 ultilizando o Split(" ");

---
+++ src/main/java/item59/Main2.java
@@ -0,0 +1,16 @@
+package item59;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		String s = "Supere seus proprios limites";
+		String[] vect = s.split(" ");
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+
+	}
+
+}
