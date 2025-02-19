4e39f59ce1f5a5032a2853beb0e03e6babdd9513
paulo <paulo@JR-LENOVO>
2025-02-19 13:56:53 -0300

    item59 Ultilizando o Split(" ");

--- src/main/java/item59/Main2.java
+++ src/main/java/item59/Main2.java
@@ -3,15 +3,18 @@ package item59;
 public class Main2 {
 
 	public static void main(String[] args) {
-		
+
+		// Declara uma string contendo uma frase
 		String s = "Supere seus proprios limites";
+
+		// Usa a função split() para dividir a string em palavras, separando pelos
+		// espaços em branco (" ")
 		String[] vect = s.split(" ");
-		System.out.println(vect[0]);
-		System.out.println(vect[1]);
-		System.out.println(vect[2]);
-		System.out.println(vect[3]);
 
-		
+		// Exibe cada palavra separadamente com base no índice do vetor resultante
+		System.out.println(vect[0]); // Supere
+		System.out.println(vect[1]); // seus
+		System.out.println(vect[2]); // proprios
+		System.out.println(vect[3]); // limites
 	}
-
 }
